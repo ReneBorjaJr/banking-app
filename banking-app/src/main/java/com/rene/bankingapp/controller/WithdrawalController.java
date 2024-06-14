85aba5e46cb684fe42a0fa667263174d8fa05bc2
KalilK13 <kalilkennedy13@gmail.com>
2024-06-14 14:47:53 -0400

    withdrawal pojo, service layer, repo and controller.

--- banking-app/src/main/java/com/rene/bankingapp/controller/WithdrawalController.java
+++ banking-app/src/main/java/com/rene/bankingapp/controller/WithdrawalController.java
@@ -1,4 +1,42 @@
 package com.rene.bankingapp.controller;
 
+import com.rene.bankingapp.domain.Withdrawal;
+import com.rene.bankingapp.service.WithdrawalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
 public class WithdrawalController {
+    @Autowired
+    private WithdrawalService withdrawalService;
+
+    @GetMapping(value="/accounts/{accountId}/withrdawals")
+    public ResponseEntity<Iterable<Withdrawal>> getAllWithdrawals() {
+        Iterable<Withdrawal> withdrawals = withdrawalService.getAllWithdrawals();
+        return new ResponseEntity<>(withdrawalService.getAllWithdrawals(), HttpStatus.OK);
+    }
+    @GetMapping(value="/withdrawals/{withdrawalId}")
+    public ResponseEntity<?> getWithdrawal(@PathVariable Long withdrawalId) {
+        Optional<Withdrawal> withdrawal = withdrawalService.getWithdrawal(withdrawalId);
+        return new ResponseEntity<> (withdrawal, HttpStatus.OK);
+    }
+    @PostMapping(value="/accounts/{accountId}/withdrawals")
+    public ResponseEntity<?> createWithdrawal(@RequestBody Withdrawal withdrawal) {
+        withdrawal = withdrawalService.save(withdrawal);
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
+    }
+    @PutMapping(value="/polls/{pollId}")
+    public ResponseEntity<?> updateWithdrawal(@RequestBody Withdrawal withdrawal, @PathVariable Long withdrawalId) {
+        Withdrawal w = withdrawalService.save(withdrawal);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @DeleteMapping(value="/withdrawals/{withdrawalId}")
+    public ResponseEntity<?> deleteWithdrawal(@PathVariable Long withdrawalId) {
+        withdrawalService.delete(withdrawalId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- banking-app/src/main/java/com/rene/bankingapp/domain/Withdrawal.java
+++ banking-app/src/main/java/com/rene/bankingapp/domain/Withdrawal.java
@@ -1,4 +1,82 @@
 package com.rene.bankingapp.domain;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 public class Withdrawal {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String type;
+    private String transaction_Date;
+    private String status;
+    private Long payer_id;
+    private String medium;
+    private Double amount;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTransaction_Date() {
+        return transaction_Date;
+    }
+
+    public void setTransaction_Date(String transaction_Date) {
+        this.transaction_Date = transaction_Date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getPayer_id() {
+        return payer_id;
+    }
+
+    public void setPayer_id(Long payer_id) {
+        this.payer_id = payer_id;
+    }
+
+    public String getMedium() {
+        return medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
