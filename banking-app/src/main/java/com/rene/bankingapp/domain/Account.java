84ec88b332163a14ab129889cf12c5fe9fd49b0c
Nickwancode <simplynick0904@gmail.com>
2024-06-13 20:03:23 -0400

    Not done I still have questions

--- banking-app/src/main/java/com/rene/bankingapp/domain/Account.java
+++ banking-app/src/main/java/com/rene/bankingapp/domain/Account.java
@@ -1,20 +1,30 @@
 package com.rene.bankingapp.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 @Entity
 public class Account {
  @Id
  @GeneratedValue
  @Column(name = "ACCOUNT_ID")
  private Long id;
-
+ @NotBlank
+ @Enumerated(EnumType.STRING)
  private String type;
+ @NotBlank
+ @Size(min = 3, max = 20)
  private String nickname;
+ @Min(0)
+ @Column(name = "member_points")
  private Integer rewards;
+ @Min(0)
  private Double balance;
+ @NotNull
+ @ManyToOne(fetch = FetchType.LAZY)
  private Customer customer;
 
  public Long getId() {
