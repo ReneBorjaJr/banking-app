8e393e18445dcd12054d5a4e1a20e88eee71d505
ReneBorjaJr <rene.borja99@gmail.com>
2024-06-16 11:56:45 -0400

    Finished a bunch of exceptions

--- banking-app/src/main/java/com/rene/bankingapp/exception/ErrorResponse.java
+++ banking-app/src/main/java/com/rene/bankingapp/exception/ErrorResponse.java
@@ -4,46 +4,15 @@ import java.time.LocalDateTime;
 
 public class ErrorResponse {
 
-    private LocalDateTime timeStamp;
-    private int status;
-    private String error;
+    private int code;
     private String message;
-    private String path;
 
-    public ErrorResponse() {
-        this.timeStamp = LocalDateTime.now();
+    public int getCode() {
+        return code;
     }
 
-    public ErrorResponse(int status, String error, String message, String path) {
-        this();
-        this.status = status;
-        this.error = error;
-        this.message = message;
-        this.path = path;
-    }
-
-    public LocalDateTime getTimeStamp() {
-        return timeStamp;
-    }
-
-    public void setTimeStamp(LocalDateTime timeStamp) {
-        this.timeStamp = timeStamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
+    public void setCode(int code) {
+        this.code = code;
     }
 
     public String getMessage() {
@@ -52,13 +21,5 @@ public class ErrorResponse {
 
     public void setMessage(String message) {
         this.message = message;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
     }
 }
