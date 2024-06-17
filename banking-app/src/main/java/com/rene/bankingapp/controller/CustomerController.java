24da429b7766d229e86245532e1be6db27ce85df
Nado0on <seifnadin1@gmail.com>
2024-06-17 15:05:58 -0500

    nadin's customer class 1st commit

--- banking-app/src/main/java/com/rene/bankingapp/controller/CustomerController.java
+++ banking-app/src/main/java/com/rene/bankingapp/controller/CustomerController.java
@@ -1,4 +1,42 @@
 package com.rene.bankingapp.controller;
 
+import com.rene.bankingapp.domain.Customer;
+import com.rene.bankingapp.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
+
+
+    @Autowired
+    private CustomerService service;
+
+    @GetMapping
+    public Iterable<Customer> getAllCustomers() {
+        return service.findAllCustomers();
+    }
+
+    @GetMapping("/{id}")
+    public Customer getCustomerById(@PathVariable Long id) {
+        return service.findCustomerById(id);
+    }
+
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return service.saveCustomer(customer);
+    }
+
+    @PutMapping("/{id}")
+    public Customer updateCustomer(@PathVariable Long id, @RequestBody Customer customer) {
+        return service.updateCustomer(id, customer);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable Long id) {
+        service.deleteCustomer(id);
+    }
+
 }
+
